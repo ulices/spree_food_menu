31b6c00e60c99aaabc7b99eb8022e88b4515f211
Jaime Victoria <jaime.victoria@crowdint.com>
2014-12-19 13:19:05 -0600

    Send comment of line item to save

--- app/decorators/controllers/spree/checkouts_controller_decorator.rb
+++ app/decorators/controllers/spree/checkouts_controller_decorator.rb
@@ -5,7 +5,7 @@ Spree::Api::CheckoutsController.class_eval do
   def add_order_line_items
     params[:order][:line_items].each do |item|
       variant = Spree::Variant.find(item[:variant_id])
-      line_item = @order.contents.add(variant, item[:quantity] || 1)
+      line_item = @order.contents.add(variant, item[:comment], item[:quantity] || 1)
       line_item.delivery_type = item[:delivery_type]
       line_item.delivery_time = item[:delivery_time].to_time
       line_item.save!
