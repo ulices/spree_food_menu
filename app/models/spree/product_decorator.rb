af74b7ef0645b12fc2d03ae9ebb8071d130d34d1
Jesus Figueroa <jesus.figueroa@crowdint.com>
2015-01-28 18:26:28 -0600

    Adds default value for products available_from and available_to

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,10 @@
 Spree::Product.class_eval do
   has_many :menu_product_days, dependent: :destroy
+
+  before_create :add_default_availability
+
+  def add_default_availability
+    self.available_from = '08:00' if self.available_from.nil?
+    self.available_to = '18:00' if self.available_to.nil?
+  end
 end
